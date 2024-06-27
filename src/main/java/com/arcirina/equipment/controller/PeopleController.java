c2eeebbf6ad754cc16cadd211eb79595f397d0df
arcirina <irinaarc0712@gmail.com>
2024-06-27 15:13:41 +0500

    Не запускается. Но почему?

--- src/main/java/com/arcirina/equipment/controller/PeopleController.java
+++ src/main/java/com/arcirina/equipment/controller/PeopleController.java
@@ -1,5 +1,7 @@
 package com.arcirina.equipment.controller;
 
+import com.arcirina.equipment.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +11,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
+    private final PersonDAO personDAO; // injection. можем получать людей
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
 
     @GetMapping()
     public String index(Model model) {
         //получаем всех людей из ДАО и передаём на отображдение в предсатвление
-        return null;
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         // получаем 1 человека по ид из дао, передаём на отобр в представление
-        return null;
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
     }
 
 
--- src/main/java/com/arcirina/equipment/dao/PersonDAO.java
+++ src/main/java/com/arcirina/equipment/dao/PersonDAO.java
@@ -1,5 +1,31 @@
 package com.arcirina.equipment.dao;
 
+import com.arcirina.equipment.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Кирилл"));
+
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId()==id).findAny().orElse(null);
+    }
 
 }
